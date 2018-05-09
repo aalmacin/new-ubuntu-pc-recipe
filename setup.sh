0e9ff47717ea87a0d71a797d5f3a820e02928478
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2018-05-09 12:31:22 +0000

    Added message commands

--- setup.sh
+++ setup.sh
@@ -2,23 +2,6 @@
 
 sudo apt-get update -y
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-NC='\033[0m'
-
-success_message () {
-  printf "$GREEN$1$NC\n"
-}
-
-failure_message () {
-  printf "$RED$1$NC\n"
-}
-
-additional_step_message () {
-  printf "$YELLOW$1$NC\n"
-}
-
 setup_vim () {
   if [ ! `which vim` ]; then
     additional_step_message "Vim does not exist. Installing..."
@@ -26,21 +9,26 @@ setup_vim () {
     success_message "Successfully installed vim"
   fi
 
-  echo 'set -o vim' >> ~/.bashrc
+  echo 'set -o vi' >> ~/.bashrc
   echo 'export EDITOR=/usr/bin/vim' >> ~/.bashrc
+
+  cat vim-configs/.vimrc >> ~/.vimrc
 
   success_message "Successfully setup vim"
 }
 
 setup_bin () {
-  echo 'PATH="'$(pwd)/bin':$PATH"' >> ~/.bashrc
+  BIN_PATH=$(pwd)/bin
+  echo 'PATH="'$BIN_PATH':$PATH"' >> ~/.bashrc
+  sudo chmod +x $BIN_PATH/*
+  success_message "Successfully setup bin"
+}
+
+setup_pip () {
+  sudo apt-get install -y python3-pip
+  pip3 install awscli
 }
 
 setup_bin
-
 setup_vim
-
-sudo apt-get install -y python3-pip
-pip3 install awscli
-
-cat vim-configs/.vimrc >> ~/.vimrc
+setup_pip
